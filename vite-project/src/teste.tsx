b15ba8f6f97d3fa6d6aadfbb097313cd178b97c5
Gabriel Simioni <gabrielsimioniwo@gmail.com>
2026-05-11 21:19:14 -0300

    i

--- vite-project/src/teste.tsx
+++ vite-project/src/teste.tsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
+import React from 'react'
 
-function App() {
-  const [item, setItem] = useState("");
-  const [lista, setLista] = useState<string[]>([]);
 
-  const adicionar = () => {
-    if (item.trim() === "") return;
-    setLista([...lista, item]);
-    setItem("");
-  };
 
+const link = ({children}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   return (
-    <div>
-      <h2>Itens: {lista.length}</h2>
-
-      <input value={item} onChange={(e) => setItem(e.target.value)} />
-      <button onClick={adicionar}>Adicionar</button>
-
-      <ul>
-        {lista.map((i, index) => (
-          <li key={index}>{i}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    <a>{children}</a>
+  )
 }
 
-export default App;
+export default link
