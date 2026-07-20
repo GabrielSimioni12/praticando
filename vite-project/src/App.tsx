e91576487e28ae9611b9dbdca224b4d67c1087dc
Gabrielsimioni12 <gabrielsimioniwo@gmail.com>
2026-07-20 17:38:30 -0300

    praticando #15

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,66 +1,49 @@
-import React, { useState } from 'react'
-import ListaFuncionarios from './ListaFuncionarios'
-import CardFuncionario from './CardFuncionario'
-import { FuncionarioContext } from './FuncionarioContext'
-import TotalSalarios from './TotalSalarios'
+import React from 'react'
+
+type Produto = {
+  id: number
+  nome: string
+  preco: number
+}
+
+
+type Action =
+  | {
+      type: 'ADICIONAR'
+      produto: Produto
+    }
+  | {
+      type: 'REMOVER'
+      id: number
+    }
+  | {
+      type: 'LIMPAR'
+    }
+
 
 const App = () => {
-
-const [funcionario, setFuncionarios] = useState([ {
-
+  const produtosIniciais: Produto[] = [
+  {
     id: 1,
-    nome: 'Gabriel',
-    cargo: 'Desenvolvedor Front-End',
-    salario: 4500
+    nome: 'Notebook',
+    preco: 4500
   },
   {
     id: 2,
-    nome: 'Beatriz',
-    cargo: 'UX Designer',
-    salario: 5200
+    nome: 'Mouse',
+    preco: 150
   },
   {
     id: 3,
-    nome: 'João',
-    cargo: 'Analista de Dados',
-    salario: 6100
-  },
-  {
-    id: 4,
-    nome: 'Maria',
-    cargo: 'Product Owner',
-    salario: 8000
-  },
-  {
-    id: 5,
-    nome: 'Pedro',
-    cargo: 'Suporte Técnico',
-    salario: 2800
-  },
-  {
-    id: 6,
-    nome: 'Ana',
-    cargo: 'Desenvolvedora Back-End',
-    salario: 7000
+    nome: 'Teclado',
+    preco: 300
   }
-])
+]
 
 
 
   return (
-    <div>
-        {funcionario.map((func) => (
-          <CardFuncionario funcionario={func} key={func.id} />
-        ))}
-
-
-        <FuncionarioContext.Provider value={{ funcionario, setFuncionarios}}>
-        <ListaFuncionarios />
-        <TotalSalarios />
-        </FuncionarioContext.Provider>
-
-      
-    </div>
+    <div>App</div>
   )
 }
 
